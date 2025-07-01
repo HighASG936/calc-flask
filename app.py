4835e464381df21c67362a2c414bb9574913dcf5
HighASG936 <lz_rock@hotmail.com>
2025-07-01 08:54:17 -0600

    Update app.oy

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from microservices.multiplier import multiplier_bp
 from microservices.subtractor import subtractor_bp
 from microservices.divider import divider_bp
 
+app = Flask(__name__)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app = Flask(__name__)
     app.register_blueprint(adder_bp)
     app.register_blueprint(multiplier_bp)
     app.register_blueprint(subtractor_bp)
