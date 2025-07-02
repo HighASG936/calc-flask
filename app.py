0a20e62d15d73d1a5d7225f962da4c3c60596339
Aurelio Siordia <lz_rock@hotmail.com>
2025-07-01 18:35:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ from microservices.divider import divider_bp
 
 app = Flask(__name__, template_folder='statics')
 
+app.register_blueprint(adder_bp)
+app.register_blueprint(multiplier_bp)
+app.register_blueprint(subtractor_bp)
+app.register_blueprint(divider_bp)
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.register_blueprint(adder_bp)
-    app.register_blueprint(multiplier_bp)
-    app.register_blueprint(subtractor_bp)
-    app.register_blueprint(divider_bp)
-
     app.run(host='0.0.0.0', port=8080)
-
